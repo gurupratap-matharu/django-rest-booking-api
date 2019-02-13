84ff86e649cde417fb1386d12f6569a82b0391db
Gurupratap <gurupratap.matharu@gmail.com>
2019-02-13 15:23:19 -0300

    updated serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -32,22 +32,7 @@ class MatchDetailSerializer(serializers.ModelSerializer):
         model = Match
         fields = ('id', 'url', 'name', 'startTime', 'sport', 'market')
 
-    # def create(self, validated_data):
-    #     message = validated_data.pop('message_type')
-    #     print("Veer the message is = ", message)
-    #     if message == "NewEvent":
-    #         match = validated_data.pop('event')
-    #         sport = match.pop('sport')
-    #         markets = match.pop('markets')
-    #         markets = markets[0]
-    #         selections = markets.pop('selections')
-    #         selections = selections[0]
-    #         s = Sport.objects.create(**sport)
-    #         market = Market.objects.create(**markets, sport=s)
-    #         for selection in selections:
-    #             market.selections.create(**selection)
-    #         mat = Match.objects.create(**match, sport=s, market=market)
-    #         return mat
+
 
 
 
